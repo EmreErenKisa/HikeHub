deed23febae5dee567e950bf6a783bd2bb07fd45
DorukArca <doruk.arca@ug.bilkent.edu.tr>
2024-05-08 01:05:14 +0300

    Added Create account class and its layout. Added Main Screen(Log in) and its layout. Added round corner for modifying buttons. Added a white x png to close forgot password frame.

--- app/src/main/java/com/example/hikehub/MainActivity.java
+++ app/src/main/java/com/example/hikehub/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.hikehub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,20 +14,37 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText email;
+    EditText password;
+    Button Signin;
+    TextView createAcc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.MainScreen), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        email = findViewById(R.id.emailTV);
+        password = findViewById(R.id.passtv);
+        Signin = findViewById(R.id.signinButton);
+        createAcc = findViewById(R.id.createNewAccount);
     }
 
-    public void login(View v){
+    public void createAccount(View v){
+        Intent i = new Intent(this, CreateAccount.class);
+        startActivity(i);
+    }
+
+    public void forgotPass(View v){
+        findViewById(R.id.forgotPassFrame).setVisibility(View.VISIBLE);
+    }
+
+    public void closeFP(View v){
+        findViewById(R.id.forgotPassFrame).setVisibility(View.INVISIBLE);
+    }
+
+    public void logintoApp(View v){
         Intent i = new Intent(this, UserScreen.class);
         startActivity(i);
     }
