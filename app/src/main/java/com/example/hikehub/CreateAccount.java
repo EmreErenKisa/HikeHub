90b767a8c5c220a88a561c27246b7f61d7b2410c
DorukArca <doruk.arca@ug.bilkent.edu.tr>
2024-05-14 21:15:30 +0300

    Added Create account class and its layout. Added Main Screen(Log in) and its layout. Added round corner for modifying buttons. Added a white x png to close forgot password frame. Fixed minor issues such as button visibility and button hint message.

--- app/src/main/java/com/example/hikehub/CreateAccount.java
+++ app/src/main/java/com/example/hikehub/CreateAccount.java
@@ -1,8 +1,12 @@
 package com.example.hikehub;
 
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +14,18 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -29,6 +42,8 @@ public class CreateAccount extends AppCompatActivity {
     double weightNumber;
     double heightNumber;
 
+    FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +58,7 @@ public class CreateAccount extends AppCompatActivity {
         height = findViewById(R.id.heightTV);
         sexM = findViewById(R.id.maleRB);
         sexF = findViewById(R.id.femaleRB);
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
@@ -57,8 +73,42 @@ public class CreateAccount extends AppCompatActivity {
         weightNumber = Double.parseDouble(weight.getText().toString());
         heightNumber = Double.parseDouble(height.getText().toString());
 
+        String emailS, passwordS, userS;
+        emailS = String.valueOf(email.getText());
+        passwordS = String.valueOf(pass.getText());
+        userS = String.valueOf(username.getText());
 
-        Account acc = new Account(email.getText().toString(),pass.getText().toString(),username.getText().toString(),isMale,ageNumber,heightNumber,weightNumber);
+        if(TextUtils.isEmpty(emailS)){
+            Toast.makeText(CreateAccount.this,"Enter email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(passwordS)){
+            Toast.makeText(CreateAccount.this,"Enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(userS)){
+            Toast.makeText(CreateAccount.this,"Enter username", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+
+        mAuth.createUserWithEmailAndPassword(emailS,passwordS)
+                .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(CreateAccount.this, "Acount created.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(CreateAccount.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        Account acc = new Account(emailS,passwordS,userS,isMale,ageNumber,heightNumber,weightNumber);
         Intent i = new Intent(this, UserScreen.class);
         startActivity(i);
     }
