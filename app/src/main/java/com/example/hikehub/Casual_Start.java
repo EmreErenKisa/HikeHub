25532e2d076d77840631a14785125f97265ac535
EmreErenKisa <emreerenkisa@gmail.com>
2024-05-15 23:54:18 +0300

    Fixed route creation.

--- app/src/main/java/com/example/hikehub/Casual_Start.java
+++ app/src/main/java/com/example/hikehub/Casual_Start.java
@@ -1,12 +1,18 @@
 package com.example.hikehub;
 
 import static android.graphics.Color.rgb;
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
 
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.Manifest;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -27,23 +33,33 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-public class Casual_Start extends SuperScreen implements OnMapReadyCallback {
+public class Casual_Start extends SuperScreen implements OnMapReadyCallback, TaskLoadedCallback {
 
     private final int FINE_PERMISSION_CODE = 1;
     private  static int timesMapClicked = 0;
     private GoogleMap gMap;
+    boolean routeStarted;
     Location currentLocation;
     FusedLocationProviderClient fusedLocationProviderClient;
     Button startRoute;
+    Button leaveRoute;
+    EditText showDistance;
+    TextView textView;
+    LatLng location;
     LatLng destination;
-    List<LatLng> points;
+    Polyline currentPolyLine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +67,11 @@ public class Casual_Start extends SuperScreen implements OnMapReadyCallback {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_casual_start);
 
+        routeStarted = false;
         startRoute = findViewById(R.id.createCasual);
+        showDistance = findViewById(R.id.casualDistance);
+        textView = findViewById(R.id.casualText);
+        leaveRoute = findViewById(R.id.leaveCasual);
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         getLastLocation();
@@ -60,17 +80,22 @@ public class Casual_Start extends SuperScreen implements OnMapReadyCallback {
             if (destination.equals(null)) {
                 Toast.makeText(this, "Please enter a destination.", Toast.LENGTH_SHORT).show();
             } else {
-                points.add(destination);
+                routeStarted = true;
+                leaveRoute.setVisibility(VISIBLE);
+                startRoute.setVisibility(INVISIBLE);
+                showDistance.setClickable(false);
+                textView.setText("Remaining Distance");
                 createRoute();
             }
         });
+
+        leaveRoute.setOnClickListener(view -> {
+
+        });
     }
 
-    private  void  createRoute() {
-        PolylineOptions polylineOptions = new PolylineOptions();
-        polylineOptions.addAll(points);
-        polylineOptions.color(rgb(255, 0, 0));
-        gMap.addPolyline(polylineOptions);
+    private void createRoute() {
+        new FetchURL(Casual_Start.this).execute("https://maps.googleapis.com/maps/api/directions/json?destination=" + destination.latitude + "," + destination.longitude + "&mode=walking&origin=" + location.latitude + "," + location.longitude + "&key=" + getString(R.string.my_API_key), "walking");
     }
 
     private void getLastLocation() {
@@ -98,21 +123,20 @@ public class Casual_Start extends SuperScreen implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
 
         gMap = googleMap;
-        LatLng location = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        points = new ArrayList<LatLng>();
-        points.add(location);
+        location = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
 
         gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
             @Override
             public void onMapClick(@NonNull LatLng latLng) {
-                timesMapClicked++;
-                if (timesMapClicked > 1) {
-                    gMap.clear();
-                    gMap.addCircle(new CircleOptions().center(location).radius(30).fillColor(rgb(255,255,255)).strokeWidth(0).visible(true));
-                    gMap.addCircle(new CircleOptions().center(location).radius(22).fillColor(rgb(50,50,255)).strokeWidth(0).visible(true));
-                }
-                destination = latLng;
-                gMap.addMarker(new MarkerOptions().position(latLng).title("Destination").visible(true));
+                    timesMapClicked++;
+                    if (timesMapClicked > 1) {
+                        gMap.clear();
+                        gMap.addCircle(new CircleOptions().center(location).radius(30).fillColor(rgb(255,255,255)).strokeWidth(0).visible(true));
+                        gMap.addCircle(new CircleOptions().center(location).radius(22).fillColor(rgb(50,50,255)).strokeWidth(0).visible(true));
+                    }
+                    destination = latLng;
+                    gMap.addMarker(new MarkerOptions().position(latLng).title("Destination").visible(true));
             }
         });
 
@@ -132,6 +156,15 @@ public class Casual_Start extends SuperScreen implements OnMapReadyCallback {
             } else {
                 Toast.makeText(this, "Location permission is denied.", Toast.LENGTH_SHORT).show();
             }
+        }
+    }
+
+    @Override
+    public void onTaskDone(Object... values) {
+        if (currentPolyLine != null) {
+            currentPolyLine.remove();
+        }else {
+            currentPolyLine = gMap.addPolyline( (PolylineOptions) values[0]);
         }
     }
 }
